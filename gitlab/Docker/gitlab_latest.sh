af7799cca38c7181ae9d0970589ac55b16336298
thinkMMP <2271404280@qq.com>
2017-11-13 18:53:59 +0800

    2017-11-13 18:53:58

--- gitlab/Docker/gitlab_latest.sh
+++ gitlab/Docker/gitlab_latest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # @Date:   2017-11-13 15:34:58
-# @Last Modified time: 2017-11-13 18:07:40
+# @Last Modified time: 2017-11-13 18:53:45
 : "gitlab latest 汉化版
 [github address](https://github.com/twang2218/gitlab-ce-zh)
 [阿里云镜像库](https://dev.aliyun.com/search.html)
@@ -10,8 +10,8 @@ docker pull registry.cn-hangzhou.aliyuncs.com/lab99/gitlab-ce-zh
 # docker run --name gitlab -p 6608:80 -it $IMAGE_ID
 docker run --detach \
     --name gitlab \
-    --publish 80:80 \
-    --publish 2266:22 \
+    --publish 22:22 \  # must
+    --publish 80:80 \  # must
     --publish 4436:443 \
     --hostname gitlab.tianxi.com \
     --volume $HOME/softwares/docker_gitlab/conf:/etc/gitlab \
