b5f2e78e50ee4de70a27bf2276bfd408f83fcc7b
thinkMMP <2271404280@qq.com>
2018-01-10 18:25:23 +0800

    2018-01-10 18:25:22

--- gitlab/Docker/gitlab.sh
+++ gitlab/Docker/gitlab.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 # @Date:   2017-11-13 15:34:58
-# @Last Modified time: 2017-12-26 16:00:09
+# @Last Modified time: 2018-01-10 13:22:39
 : "gitlab latest 汉化版
 [github address](https://github.com/twang2218/gitlab-ce-zh)
 [阿里云镜像库](https://dev.aliyun.com/search.html)
     lab99/gitlab-ce-zh
 "
 sudo docker pull registry.cn-hangzhou.aliyuncs.com/lab99/gitlab-ce-zh
-# docker run --name gitlab -p 6608:80 -it $IMAGE_ID
-sudo docker run --detach \
+# sudo docker run --name gitlab -p 6608:80 -it $IMAGE_ID
+sudo docker run \
+    --detach \
     --name gitlab \
-    --publish 80:80 \  # must
+    --publish 8001:80 \  # must
     --publish 2222:22 \  # 需要配置.ssh/config
     --publish 4433:443 \
     --hostname gitlab.tianxi.com \
-    --volume $HOME/USER_SOFTWARES/docker/gitlab/conf:/etc/gitlab \
-    --volume $HOME/USER_SOFTWARES/docker/gitlab/logs:/var/log/gitlab \
-    --volume $HOME/USER_SOFTWARES/docker/gitlab/data:/var/opt/gitlab \
+    --volume $HOME/softwares/docker/gitlab/conf:/etc/gitlab \
+    --volume $HOME/softwares/docker/gitlab/logs:/var/log/gitlab \
+    --volume $HOME/softwares/docker/gitlab/data:/var/opt/gitlab \
     --restart=always \
     $IMAGE_ID
--- setUbuntu/main/mysql.sh
+++ setUbuntu/main/mysql.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # @Date:   2017-05-09 20:51:14
-# @Last Modified time: 2018-01-07 10:54:05
+# @Last Modified time: 2018-01-10 17:08:20
 echo $user_password | sudo -S echo -e "\033[1;;42m\n\033[0m"
 
 mysql --version || sudo apt-get -y install mysql-server mysql-client
 
-: "mysql -u root -p
-jetbrains-->CTRL+E-->Database-->下载驱动包-->+Data Source
-或者mysql-workbench-community(官网)
-"
+: "sudo vim /etc/mysql/my.cnf"
