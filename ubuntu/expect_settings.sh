3da95499e4dc4d5ef8299fc96d53082686fddb29
forgetIt <2271404280@qq.com>
2018-04-27 18:56:34 +0800

    2018-04-27 18:56:34

--- ubuntu/expect_settings.sh
+++ ubuntu/expect_settings.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 # @Date:   2018-03-30 13:42:38
-# @Last Modified time: 2018-04-11 11:28:20
+# @Last Modified time: 2018-04-27 18:24:17
 # if [ $1 ]; then
 # else
 #     echo "need password"
--- ubuntu/source_installer.sh
+++ ubuntu/source_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # @Date:   2017-04-01 14:27:49
-# @Last Modified time: 2018-04-12 15:39:32
+# @Last Modified time: 2018-04-27 18:54:32
 : <<'COMMENT'
 自动搭建 Ubuntu16.04LTS 开发环境
 设置 -> 软件和更新 -> 下载自
@@ -18,28 +18,6 @@ echo ${PASSWORD} | sudo -S echo "start"
 (
     [[ -f ${ROOT_CONFIG_FILE} ]] || touch ${ROOT_CONFIG_FILE}
     ) && echo "${ROOT_CONFIG}" | sudo tee ${ROOT_CONFIG_FILE}
-# ***************************************************************
-# ppa
-source_count=1
-which apt-fast || (
-    sudo add-apt-repository -y ppa:saiarcot895/myppa
-    let source_count++
-    )
-which atom || (
-    sudo add-apt-repository -y ppa:webupd8team/atom
-    let source_count++
-    )
-uget-gtk --version || (
-    sudo add-apt-repository -y ppa:t-tujikawa/ppa
-    sudo add-apt-repository -y ppa:plushuang-tw/uget-stable
-    let source_count++
-    )
-
-[[ ${source_count} == 1 ]] || sudo apt-fast update
-# ***************************************************************
-which apt-fast || sudo apt-get -y install apt-fast
-git --version  || sudo apt-fast -y install git
-apt-fast --version | cat | head -n 2
 
 subl -v || (
     cd ${PATH_SOFTWARES} \
@@ -55,6 +33,28 @@ read -p "Rewrite sublime settings? [Y/n]" var && (
     && cp -rf ${PATH_DEVENV}/editor/sublime/* ${sublime_conf_path}
 )
 # ***************************************************************
+# ppa
+source_count=1
+which apt-fast || (
+    sudo add-apt-repository -y ppa:apt-fast/stable
+    let source_count++
+    )
+which atom || (
+    sudo add-apt-repository -y ppa:webupd8team/atom
+    let source_count++
+    )
+uget-gtk --version || (
+    sudo add-apt-repository -y ppa:t-tujikawa/ppa
+    sudo add-apt-repository -y ppa:plushuang-tw/uget-stable
+    let source_count++
+    )
+
+[[ ${source_count} == 1 ]] || sudo apt-get update
+which apt-fast || sudo apt-get -y install apt-fast
+apt-fast --version | cat | head -n 2
+git --version  || sudo apt-fast -y install git
+
+# ***************************************************************
 ssh -V      || sudo apt-fast -y install openssh-server
 nginx -v    || sudo apt-fast -y install nginx
 expect -v   || sudo apt-fast -y install expect  # 自动交互
@@ -66,8 +66,8 @@ which unrar || sudo apt-fast -y install unrar  # unrar e xxx
 which conky || sudo apt-fast -y install conky
 which docky || sudo apt-fast -y install docky
 zsh        --version || sudo apt-fast -y install zsh
-wine       --version || sudo apt-fast -y install wine
 tree       --version || sudo apt-fast -y install tree
+wine       --version || sudo apt-fast -y install wine-stable
 mysql      --version || sudo apt-fast -y install mysql-server mysql-client
 mongo      --version || sudo apt-fast -y install mongodb-server mongodb-clients
 uget-gtk   --version || sudo apt-fast -y install aria2 uget
@@ -76,22 +76,22 @@ terminator --version || sudo apt-fast -y install terminator
 # ***************************************************************
 pip      --version || sudo apt-fast -y install python-pip
 pip3     --version || sudo apt-fast -y install python3-pip
-ipython  --version || sudo apt-fast -y install ipython ipython-notebook
-ipython3 --version || sudo apt-fast -y install ipython3 ipython3-notebook
-bpython --version || pip install bpython
+ipython  --version || sudo apt-fast -y install ipython
+ipython3 --version || sudo apt-fast -y install ipython3
+bpython  --version || sudo apt-fast -y install bpython
+bpython3 --version || sudo apt-fast -y install bpython3
+# ipython-notebook
+# ipython3-notebook
 virtualenv --version || sudo pip install virtualenv -i ${PIP_SOURCE}
 
-python -c "import Tkinter; exit()" || sudo apt-fast -y install python-tk
-python3 -c "import tkinter; exit()" || sudo apt-fast -y install python3-tk
-
-python -c "import MySQLdb;exit()" || (
-    sudo apt-fast -y install libmysqlclient-dev python-mysqldb
-)
+python  -c "import Tkinter;exit()" || sudo apt-fast -y install python-tk
+python3 -c "import tkinter;exit()" || sudo apt-fast -y install python3-tk
+python  -c "import MySQLdb;exit()" || sudo apt-fast -y install libmysqlclient-dev python-mysqldb
 
 dpkg-query -S python-dev || sudo apt-fast -y install python-dev
 dpkg-query -S python3-dev || sudo apt-fast -y install python3-dev
-python -c "import PyQt5;exit()" || sudo apt-fast -y install python-pyqt5
 pip3 list | grep QScintilla || pip3 install QScintilla -i ${PIP_SOURCE}
+python  -c "import PyQt5;exit()" || sudo apt-fast -y install python-pyqt5
 # ***************************************************************
 read -p "To configure ssh key? [Y/n]" var && (
     [[ "${var}" == "Y" ]] \
@@ -113,7 +113,6 @@ read -p "To configure vim? [Y/n]" var && (
 )
 
 # oh-my-zsh
-oh_my_zsh_repository=https://raw.github.com/robbyrussell/oh-my-zsh
 [[ ${SHELL} != /usr/bin/zsh ]] \
-&& echo ${ENV_PASSWORD} | chsh -s `which zsh` \
-&& curl -L "${oh_my_zsh_repository}/master/tools/install.sh" | sh
+&& echo ${PASSWORD} | chsh -s `which zsh` \
+&& curl -L "https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh" | sh
