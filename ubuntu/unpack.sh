f6a21504e433e69d4d99d80ac7efa54ed958aa1f
forgetIt <2271404280@qq.com>
2018-04-30 02:58:47 +0800

    2018-04-30 02:58:47

--- ubuntu/unpack.sh
+++ ubuntu/unpack.sh
@@ -4,10 +4,14 @@
 source config && echo ${PASSWORD} | sudo -S echo -e "\033[1;;42m\n\033[0m"
 
 function jetbrains() {
-    cd ${PATH_SOFTWARES} \
-    && [[ -d "./${package}" ]] || mkdir "./${package}" \
-    && tar -zxvf "${package}.tar.gz" -C "${package}" --strip-components 1 \
-    && sudo ln -sf "${package}/bin/${entry}" "/usr/bin/${name}"
+    name=${1};package=${2};entry=${3}
+    cd ${PATH_SOFTWARES} && (
+        [[ -d ${package} ]] || mkdir ${package}
+    ) && tar -zxvf "${package}.tar.gz" -C ${package} --strip-components 1 \
+    && sudo ln -sf "${package}/bin/${entry}" "/usr/bin/${name}" \
+    && cd ${HOME}/.${package}*/config && (
+        find ./ -name keymaps -type d | grep "keymaps" || mkdir keymaps
+    ) && cp -f "${PATH_DEVENV}/editor/JetBrains.xml" ./keymaps/JetBrains.xml
 }
 
 jetbrains "pycharm"  "PyCharm"  "pycharm.sh"
@@ -18,9 +22,9 @@ jetbrains "pycharm"  "PyCharm"  "pycharm.sh"
 # https://npm.taobao.org/mirrors/node/v8.9.3/node-v8.9.3-linux-x64.tar.xz
 node -v && npm -v || (
     cd ${PATH_SOFTWARES} \
-    && xz -d node.tar.xz \
+    && xz -kd node*.tar.xz \
     && [[ -d node ]] || mkdir node \
-    && tar -xvf node.tar -C node --strip-components 1
+    && tar -xvf node*.tar -C node --strip-components 1
     && sudo ln -sf node/bin/node /usr/bin/node \
     && sudo ln -sf node/bin/npm /usr/bin/npm \
     && npm config set prefix /usr/local
@@ -29,10 +33,10 @@ node -v && npm -v || (
 # npm全局命令安装目录：${prefix}/bin/
 # ***************************************************************
 # https://studygolang.com/dl/golang/go1.9.2.linux-amd64.tar.gz
-go version || (
+go version && (
     cd ${PATH_SOFTWARES} \
     && [[ -d go ]] || mkdir go \
-    && tar -zxvf go.tar.gz -C go --strip-components 1 \
+    && tar -zxvf go*.tar.gz -C go --strip-components 1 \
     && sudo ln -sf go/bin/go /usr/bin/go
     && export GOROOT=${HOME}/softwares/go \
     && export GOPATH=${HOME}/gocode \
@@ -54,7 +58,7 @@ go version || (
     grep ${GOPATH} /etc/profile || tee -e ${GOPATH}
 )
 # ***************************************************************
-sudo find ${HOME}/.config/ -name mongobooster | grep mongobooster || (
+sudo find ${HOME}/.config/ -name mongobooster | grep mongobooster && (
     cd ${PATH_SOFTWARES} \
     && axel -n 16 "http://s3.mongobooster.com/download/3.5/mongobooster-3.5.5-x86_64.AppImage" \
     && chmod +x ./mongobooster*.AppImage \
